774bde672e1a8871ef9443bf8ec3e1e17081e159
jcmassaro <49561088+jcmassaro@users.noreply.github.com>
2021-03-10 17:16:26 -0500

    Update COVID_Figure.py

--- COVID_Figure.py
+++ COVID_Figure.py
@@ -1,3 +1,4 @@
+#Imports needed to get data and turn into a bar chart
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -5,23 +6,25 @@ from matplotlib.pyplot import figure
 import matplotlib as mpl
 import matplotlib.ticker as ticker
 
+#Accessing CSV downloaded from CDC.gov
 os.chdir('/users/johnmassaro/desktop')
 df = pd.read_csv('US-cases.csv', skiprows = 3)
+
+#focusing on States/Territories and Cases, ignoring other data points
 data = {'State/Territory': df['State/Territory'], 'Total Cases': df['Total Cases']}
 df1 = pd.DataFrame(data)
 x = df1['State/Territory']
 y = df1['Total Cases']
+
+#Dropping totals at end so they don't skew chart
 y.drop(df.tail(1).index,inplace=True)
 x.drop(df.tail(1).index,inplace=True)
 
 
-
+#Creating chart
 figure(num = None, figsize =(30,15), dpi = 100, facecolor = 'white', edgecolor = 'white')
 
-
-
 plt.bar(x, height = y, color = (0.9, 0, 0, 0.8))
-#ax.get_yaxis().set_major_formatter(mpl.ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
 plt.xticks(rotation = 90)
 plt.title('COVID Cases by State', fontsize = 20)
 plt.xlabel('State')
@@ -31,7 +34,7 @@ plt.ticklabel_format(style='plain', axis='y', useLocale = True)
 
 
 
-
+#Line to save figure
 #plt.savefig('Covid_Figure2.jpg')
 
 plt.show()
